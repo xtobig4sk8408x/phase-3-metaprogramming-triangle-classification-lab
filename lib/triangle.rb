56add4f3edd74712cf5dca2eb4e6775e6eb0ca82
Theodore John Josef <john.josef@aol.com>
2023-01-16 00:44:27 -0800

    commit tree 0622b2ab99fd664cd12cd153ad2df70dd05c62e8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene 
+  attr_reader :side1, :side2, :side3 
+
+  def initialize(side1, side2, side3)
+    @side1 = side1    
+    @side2 = side2    
+    @side3 = side3
+  end
+
+  def kind 
+    validate_triangle 
+    if side1= side2 && side2 = side3
+      :equilateral 
+    elsif side1 == side2 || side2 == side3 || side1 == side3
+      :isosceles 
+    else 
+      :scalene 
+    end
+  end 
+
+  def greater_than_zero? 
+    [side1, side2, side3].all?(&:positive?)
+  end
+
+  def valid_inequalities? 
+    side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
+  end
+  
+  def validate_triangle 
+    raise TriangleError unless greater_than_zero? && valid_inequalities?  
+  end
+
+  class TriangleError < StandardError 
+  end
 end
